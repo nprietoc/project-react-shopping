c5581972fbb5988ba8b8f752ac50cc1866f102b0
Natalia Prieto <natyprietoc@hotmail.com>
2021-02-09 17:26:24 -0500

    First commit: *added a component *remove logod and images

---
+++ src/componentes/Componente.titulo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComponenteTitulo = () => {
+    let titulo = "Shopping"; 
+
+    return(
+        <h1 id="titulo">{titulo}</h1>
+    );
+}
+
+export default ComponenteTitulo;
